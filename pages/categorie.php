2d8f3d7e4d43a39f5abd53ba0e9e214795990bf8
aguinot58 <aymeric.guinot@gmail.com>
2022-06-17 10:48:14 +0200

    correctif nom fichiers CSS page catégorie et jeux

--- pages/categorie.php
+++ pages/categorie.php
@@ -28,8 +28,8 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="../css/categorie.css">
-        <link rel="stylesheet" href="../css/Header_footer.css">
+        <link rel="stylesheet" href="./../css/categorie.css">
+        <link rel="stylesheet" href="./../css/header_footer.css">
         <link rel="shortcut icon" type="image/ico" href="./../img/icone.png">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
